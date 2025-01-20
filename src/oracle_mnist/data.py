b9ec518648e2eb0b137cdc8ead9a2d920cb9eb25
astridlh <75378190+astridlh@users.noreply.github.com>
2025-01-20 12:12:38 +0100

    feat: print kwags

--- src/oracle_mnist/data.py
+++ src/oracle_mnist/data.py
@@ -257,6 +257,7 @@ class OracleMNISTBaseModule(ABC, pl.LightningDataModule):
         """
         assert self.train_dataset is not None, "Val dataset is not set"
         kwargs = dict(shuffle=False) | self.dataloader_kwargs
+        print(kwargss)
         return DataLoader(self.val_dataset, **kwargs)
 
     def test_dataloader(self):
