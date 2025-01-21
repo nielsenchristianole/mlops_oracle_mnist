fde6086b6829b9ad8c1b1b69a6ef2297e28f6d11
Christian Ole Nielsen <nielsenchristianole@gmail.com>
2025-01-21 16:23:16 +0100

    fix arg bug

--- src/oracle_mnist/utils/data_utils.py
+++ src/oracle_mnist/utils/data_utils.py
@@ -1,4 +1,4 @@
 import numpy as np
-def normalize_data(self, data: np.ndarray) -> np.ndarray:
+def normalize_data(data: np.ndarray) -> np.ndarray:
     # Normalizing the intensities of the image to [0, 1].
     return (data / 255.0).astype(np.float32)
